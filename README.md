3379306b059f1c44c2358fb69ef6b54af5a8ef0f
landfiyat <162441323+landfiyat@users.noreply.github.com>
2024-07-24 05:56:58 -0400

    size, qr options

--- README.md
+++ README.md
@@ -15,5 +15,46 @@ Dropit simplifies the process of sharing files across multiple devices, includin
 To share files with Dropit, simply run the following command in your terminal:
 
 ```bash
-dropit [--password <password>] [--geturl] [--getqr]
+dropit [--password <password>] [--geturl] [--getqr] [--maxsize <integer>]
+```
 
+*Options*
+```
+--password: <password>: Secures your file sharing session with basic authentication.
+--geturl: Prints the URL to access Dropit from the other devices.
+--getqr: Displays a QR code in the terminal, which can be scanned to connect to Dropit.
+--maxsize <size_in_GB>: Sets a maximum file size for uploads (default is 2GB).
+```
+**NOTE**: The default username is `admin`
+
+*Accessing Dropit:*
+
+Open a web browser on any device connected to the same network and enter the URL displayed in the terminal. If a password is set, you will be prompted to enter it. 
+
+
+
+
+### Additional Sections
+
+#### Configuration Options
+Detail other configuration settings if available, such as changing the default upload folder.
+
+
+## Troubleshooting
+
+- **Connection Issues**: Ensure all devices are on the same network. Check firewall settings if devices cannot connect to the server.
+- **Performance Issues**: For large file transfers, ensure the server machine has sufficient resources. Consider increasing the system limits if uploads fail due to file size.
+- **Mobile Device Compatibility**: Some mobile devices might experience difficulties accessing `http` URLs.
+
+
+
+## Contributing
+
+Contributions are welcome! If you have improvements or bug fixes, please open a pull request. For major changes, please open an issue first to discuss what you would like to change.
+Please ensure to update tests as appropriate.
+
+
+## Contributing
+
+Contributions are welcome! If you have improvements or bug fixes, please open a pull request. For major changes, please open an issue first to discuss what you would like to change.
+Please ensure to update tests as appropriate.
--- dropit/main.py
+++ dropit/main.py
@@ -1,30 +1,32 @@
 #!/usr/bin/env python3
 import os
 import socket
+import logging
 import argparse
 from functools import wraps
 from flask import Flask, request, render_template, redirect, url_for, send_from_directory
 from flask_basicauth import BasicAuth
 import time
-from gen_qr import create_qr_in_terminal
+import qrcode
 
-# Setting up command line arguments for basic authentication
 parser = argparse.ArgumentParser(description='File server with optional basic authentication.')
 parser.add_argument('--password', help='Set the password for basic authentication.', default=None)
 parser.add_argument('--geturl', action='store_true', help='Print the URL')
 parser.add_argument('--getqr', action='store_true', help='Display a QR code')
+parser.add_argument('--maxsize', type=int, default=2, help='Maximum file upload size in GB (default: 2GB)')
 args = parser.parse_args()
+
 
 app = Flask(__name__)
 home_path = os.path.expanduser('~/sharex/')
 app.config['UPLOAD_FOLDER'] = home_path
-app.config['MAX_CONTENT_LENGTH'] = 1000 * 1024 * 1024  # Maximum file upload size of 1GB
-
+GB_SIZE = args.maxsize * 1024 * 1024 * 1024  
+app.config['MAX_CONTENT_LENGTH'] = GB_SIZE  
 app.config['BASIC_AUTH_USERNAME'] = 'admin'
 app.config['BASIC_AUTH_PASSWORD'] = args.password
 app.config['BASIC_AUTH_FORCE'] = bool(args.password)
-
 basic_auth = BasicAuth(app)
+
 
 def optional_auth(f):
     """Decorator to enforce basic authentication conditionally."""
@@ -58,13 +60,13 @@ def index():
 def format_size(size_bytes):
     """Converts a size in bytes to a more human-readable form."""
     if size_bytes < 1024:
-        return size_bytes, 'B'  # Bytes
+        return size_bytes, 'B'  
     elif size_bytes < 1024 ** 2:
-        return round(size_bytes / 1024, 2), 'KB'  # Kilobytes
+        return round(size_bytes / 1024, 2), 'KB'  
     elif size_bytes < 1024 ** 3:
-        return round(size_bytes / 1024 ** 2, 2), 'MB'  # Megabytes
+        return round(size_bytes / 1024 ** 2, 2), 'MB'  
     else:
-        return round(size_bytes / 1024 ** 3, 2), 'GB'  # Gigabytes
+        return round(size_bytes / 1024 ** 3, 2), 'GB'  
 
 @app.route('/files/<filename>')
 @optional_auth
@@ -91,12 +93,26 @@ def get_ip():
         s.close()
     return IP
 
-def print_colored_ip(ip, port, lag):
+
+def create_qr_in_terminal(text):
+    qr = qrcode.QRCode(
+            version = 1,
+            error_correction=qrcode.constants.ERROR_CORRECT_L,
+            box_size=10,
+            border=4,
+        )
+    qr.add_data(text)
+    qr.make(fit=True)
+    qr.print_ascii(invert= True)
+
+
+def print_colored_ip(ip, port, lag, cl=True):
     """Prints the IP address in the terminal with changing colors."""
     os.system('cls' if os.name == 'nt' else 'clear')
     colors = ["\033[1;32m", "\033[1;34m", "\033[1;31m", "\033[1;33m", "\033[1;35m", "\033[1;36m"]
     for color in colors:
-        os.system('cls' if os.name == 'nt' else 'clear')  
+        if cl:
+            os.system('cls' if os.name == 'nt' else 'clear')  
         print(f"{color}The URL to enter on your other device connected to the same wifi network is: http://{ip}:{port}\033[0m")
         time.sleep(lag)  
     print("Starting the server. Please navigate to the URL shown above on your devices.")
@@ -105,17 +121,22 @@ def run_app():
     """Starts the Flask application."""
     ip = get_ip()
     port = 5001
+    server_url = f"http://{ip}:{port}"
     if not os.path.exists(app.config['UPLOAD_FOLDER']):
         os.makedirs(app.config['UPLOAD_FOLDER'])
     
     if args.getqr:
-        txt = f"http://{ip}:{port}"
-        create_qr_in_terminal(txt)
-    elif args.geturl:
-        print_colored_ip(ip, port, 0.5)
-    print_colored_ip(ip, port, 0.1)
+        create_qr_in_terminal(server_url)
+    if args.geturl:
+        print_colored_ip(ip, port, 0.5, cl=False)
+        # cl = False ; To avoid clearing the QR code if printed.
+    print("\033[1;32mReady! Access the server at: \033[0m" + server_url)
+
+
+    log = logging.getLogger('werkzeug')
+    log.disabled = True
+    app.logger.disabled = True
     app.run(host='0.0.0.0', port=port, debug=False)
 
 if __name__ == '__main__':
     run_app()
-
